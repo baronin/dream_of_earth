179666a37ec5d51bc52bc2a15934d9646afd4c83
Yaroslav Kirakosian <yaroslav.kirakosian@beetroot.com.ua>
2021-04-18 19:30:00 +0300

    chore

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,13 @@ import { GetServerSideProps } from "next";
 import { QueryClient, useQueryClient } from "react-query";
 import { dehydrate } from "react-query/hydration";
 
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import TopUsers from "../components/TopUsers";
 import { Dream } from "../model/Vimeo";
 import styles from "../styles/Home.module.css";
 import { apiGetAmbassadorDreams, apiGetDreams } from "../utilities/api/videos";
+import UserList from "../components/UserList/UserList";
 
 const Home: React.FC = () => {
   const queryClient = useQueryClient();
@@ -12,7 +16,11 @@ const Home: React.FC = () => {
   const videos = queryClient.getQueryData<Dream[]>("dreams");
 
   return (
-    <div className={styles.container}>
+    <div className={styles.app}>
+      <Header />
+      <Hero />
+      <TopUsers />
+      <UserList />
       <p>{featuredVideos?.[0].id}</p>
       <p>{featuredVideos?.[0].sentBy}</p>
       <p>{featuredVideos?.[0].location}</p>
