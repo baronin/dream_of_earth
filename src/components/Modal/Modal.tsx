576c4a968c184b9dace21e4233b54d54a5837d4f
Yaroslav Kirakosian <yaroslav.kirakosian@beetroot.com.ua>
2021-04-19 21:00:08 +0300

    Added component Modal

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+
+import css from "./Modal.module.css";
+
+type Props = {
+  active: boolean | undefined;
+  setActive: () => void;
+  children: string|JSX.Element|Array<string|JSX.Element>;
+};
+
+const Modal = (props: Props) => {
+  const { active, setActive, children} = props;
+  useEffect(() => {
+    document.body.style.overflow = active ? "hidden" : "";
+  });
+  return (
+    <div className={active ? `${css.overlay} ${css.overlayActive}` : css.overlay} onClick={() => setActive(false)}>
+      <div className={css.modal} onClick={(e) => e.stopPropagation()}>
+        <h2>modal</h2>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
