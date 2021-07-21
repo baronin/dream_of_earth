46f9fce520f5527f14ad3eb7615a8ae958ab339c
Yaroslav <baronin.yaroslav@gmail.com>
2021-07-21 14:27:45 +0300

    change firebase config

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,16 +1,16 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCsCKnfpfgyH5ehGPcshMVdE1UNH-CXFSo",
-  authDomain: "dream-of-earth.firebaseapp.com",
-  projectId: "dream-of-earth",
-  storageBucket: "dream-of-earth.appspot.com",
-  messagingSenderId: "785954317476",
-  appId: "1:785954317476:web:45c4b624bacc00043b3231",
+const config = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 if (firebase.apps.length === 0) {
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(config);
 }
 
 export default firebase;
