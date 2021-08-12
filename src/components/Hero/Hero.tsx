2d37c42f82ac92f6e6a1b03f3ae229e1c3f44102
Yaroslav <baronin.yaroslav@gmail.com>
2021-08-12 22:48:07 +0300

    work with fetch dataUserId for Vimeo

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,12 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import DreamWizard from "../DreamWizard/DreamWizard";
 import Modal from "../Modal";
 import css from "./Hero.module.css";
 
+const ACCESS_TOKEN = "47eb604b883b7693c7a80131f18d4d10"; // yaroslav.baronin@outlook.com
+
 const Hero = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  // hN7Dte3z5OUMyotwVNhh3GxQjw17GTtUFWcn
+  // 32b73487c41b60da5a4907849094d416
+  // user145921040
+  const [dataUser, setDataUser] = useState([]);
+  useEffect(() => {
+    async function fetchMyAPI() {
+      let response = await fetch("https://api.vimeo.com/videos", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `bearer ${ACCESS_TOKEN}`,
+          Accept: " application/vnd.vimeo.user+json;version=3.0,application/vnd.vimeo.video+json;version=3.4",
+        },
+      });
+      response = await response.json();
+      setDataUser(response);
+    }
 
+    fetchMyAPI();
+  }, []);
+
+  console.log("dataUser", dataUser, dataUser.uri);
   return (
     <section className={css.hero}>
       <div className={`${css.heroContainer} container`}>
